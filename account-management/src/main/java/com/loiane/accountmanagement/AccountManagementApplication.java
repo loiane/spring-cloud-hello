750653c6c6d8d2c45aac456e4dc18d8dfb06d3c6
Loiane <loianeg@gmail.com>
2022-11-25 19:13:01 -0500

    added user service and account management

---
+++ account-management/src/main/java/com/loiane/accountmanagement/AccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.loiane.accountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementApplication.class, args);
+	}
+
+}
---
+++ user-service/src/main/java/com/loiane/userservice/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.loiane.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String getUser() {
+        return "User is working.";
+    }
+}
